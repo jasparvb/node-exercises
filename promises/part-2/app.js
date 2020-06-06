63d2dd0d50e3c67166cd347dd1a0076ec9ca8b6a
von Buelow <jaspar@UMICH.EDU>
2020-06-06 19:08:48 -0400

    Display next card on click

--- promises/part-2/app.js
+++ promises/part-2/app.js
@@ -1,24 +1,37 @@
 $(function() {
+  let baseURL = "https://deckofcardsapi.com/api/deck"
+  //Get one card from new deck
+  $.getJSON(`${baseURL}/new/draw/?count=1`)
+    .then(data => {
+      console.log(data.cards[0].value, ' of ', data.cards[0].suit)
+    });
+    
+  //Get two cards from new deck
+  let card1 = '';
+  $.getJSON(`${baseURL}/new/draw/?count=1`)
+    .then(data => {
+      card1 = `${data.cards[0].value} of ${data.cards[0].suit}`
+      return $.getJSON(`${baseURL}/${data.deck_id}/draw/?count=1`)
+    })
+    .then(data => {
+      let card2 = `${data.cards[0].value} of ${data.cards[0].suit}`
+      console.log(card1);
+      console.log(card2);
+    });
 
-//Get one card from new deck
-$.getJSON("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
-  .then(data => {
-    console.log(data.cards[0].value, ' of ', data.cards[0].suit)
+  //Draw card and display on page
+  let deckId = null;
+  $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
+    deckId = data.deck_id;
   });
-  
-//Get two cards from new deck
-let card1 = '';
-$.getJSON("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
-  .then(data => {
-    card1 = `${data.cards[0].value} of ${data.cards[0].suit}`
-    return $.getJSON(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`)
-  })
-  .then(data => {
-    let card2 = `${data.cards[0].value} of ${data.cards[0].suit}`
-    console.log(card1);
-    console.log(card2);
+
+  $('button').on('click', function() {
+    $.getJSON(`${baseURL}/${deckId}/draw/?count=1`)
+      .then(data => {
+        $('#card-area').append(`<img src="${data.cards[0].image}">`);
+        if(data.remaining === 0) {
+          $('button').hide();
+        }
+      })
   });
-
-//Draw card and display on page
-
 });
